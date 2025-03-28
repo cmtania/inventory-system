2f704066bad8b72a047fb05a611c2ffdacaddae7
tania.dev <taniachristian01@gmail.com>
2025-03-29 00:28:40 +0800

    Implement form control array as storage

--- inventory-system-spa/src/app/modules/components/transaction/transaction.component.ts
+++ inventory-system-spa/src/app/modules/components/transaction/transaction.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { HideSpinner, ShowSpinner } from '../../state-management/actions/spinner.action';
 import { Store } from '@ngxs/store';
-import { take, tap, finalize } from 'rxjs';
+import { take, tap, finalize  } from 'rxjs';
 import { ProductModel } from '../../model/product.model';
 import { InventoryModel } from '../../model/inventory.model';
 import { InventoryService } from '../../services/inventory.service';
@@ -18,18 +18,18 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class TransactionComponent implements OnInit {
 
   transactionForm: FormGroup;
-  // inventoryItems = [
-  //   // Example inventory items
-  //   { ProductId: 1, ProductCode: 'P001', ProductName: 'Product 1', Brand: 'Brand A', Category: 'Category X', UnitPrice: 100 },
-  //   { ProductId: 2, ProductCode: 'P002', ProductName: 'Product 2', Brand: 'Brand B', Category: 'Category Y', UnitPrice: 200 }
-  // ];
   constructor(
     private fb: FormBuilder,
     private readonly _productService: ProductService,
     private readonly _store: Store,
     private readonly _inventoryService: InventoryService){
       this.transactionForm = this.fb.group({
-        carts: this.fb.array([])
+        carts: this.fb.array([]),
+        products: this.fb.array([]),
+        details: this.fb.group({
+          ItemCount: [0],
+          Total: [0],
+        })
       });
   }
 
@@ -39,7 +39,6 @@ export class TransactionComponent implements OnInit {
 
   transaction = new Transaction( new Array<Cart>());
 
-
   ngOnInit(): void {
     this.loadInventory();
   }
@@ -48,23 +47,47 @@ export class TransactionComponent implements OnInit {
     return this.transactionForm.get('carts') as FormArray;
   }
 
-  addToCart(item: InventoryModel): void {
+  get products(): FormArray {
+    return this.transactionForm.get('products') as FormArray;
+  }
+
+  get details(): FormGroup {
+    return this.transactionForm.get('details') as FormGroup;
+  }
+
+  addToCart(item: any, index: number): void {
     console.log("item", item);
+
+    const unitPrice = this.getProductValue(index, 'UnitPrice');
+    const quantity = this.getProductValue(index, 'Quantity');
+    console.log("unitPrice", unitPrice);
+    console.log("quantity", quantity);
 
     const selectedItem = this.fb.group({
       ItemId: [this.carts.length + 1],
-      ProductName: [item.ProductName],
-      Description: [item.ProductDescription],
-      Quantity: [1, Validators.required],
-      UnitPrice: [item.UnitPrice],
-      RowTotal: [(item.UnitPrice * 1)]
+      ProductName: [this.getProductValue(index, 'ProductName')],
+      Description: [this.getProductValue(index, 'Description')],
+      Quantity: [quantity, Validators.required],
+      UnitPrice: [unitPrice],
+      RowTotal: [(+unitPrice * +quantity)]
     });
 
     this.carts.push(selectedItem);
+
+    this.calculateTotal();
   }
 
   deleteCartItem(index: number): void {
     this.carts.removeAt(index);
+    this.calculateTotal();
+  }
+
+  quantityChange(index: number): void {
+    const control = this.carts.at(index).get('Quantity');
+    const unitPrice = this.carts.at(index).get('UnitPrice');
+    const rowTotal = control?.value * unitPrice?.value;
+    this.carts.at(index).get('RowTotal')?.setValue(rowTotal);
+    this.calculateTotal();
   }
   
   decreaseQuantity(index: number): void {
@@ -109,6 +132,20 @@ export class TransactionComponent implements OnInit {
          console.log("resp", resp);
          if (resp.IsOk) {
            this.inventoryItems = resp.Results[0];
+            this.inventoryItems.forEach((item: InventoryModel) => {
+              const productGroup = this.fb.group({
+                ProductId: [item.ProductId],
+                ProductCode: [item.ProductCode],
+                ProductName: [item.ProductName],
+                Description: [item.ProductDescription],
+                Supplier: [item.Supplier],
+                Quantity: [1, Validators.required],
+                Brand: [item.Brand],
+                Category: [item.Category],
+                UnitPrice: [item.UnitPrice]
+              });
+              this.products.push(productGroup);
+            });
 
            return;
          }
@@ -118,4 +155,37 @@ export class TransactionComponent implements OnInit {
        })
      ).subscribe();
    }
+
+   get getTotal(): number {
+    let total = 0;
+    this.carts.controls.forEach((item: any) => {
+      const quantity = item.get('Quantity').value;
+      const unitPrice = item.get('UnitPrice').value;
+      total += quantity * unitPrice;
+    });
+    return total;
+   }
+
+   getProductValue(index: number, controlName: string): string {
+    const control = this.products.at(index).get(controlName);
+
+    return control ? control.value : "";
+  }
+
+  getDetailsValue(controlName: string): string {
+    const control = this.details.get(controlName);
+
+    return control ? control.value : "";
+  }
+  
+  calculateTotal(): void {
+    console.log("calculateTotal");
+    const total = this.carts.controls.reduce((sum, control) => {
+      const rowTotal = control.get('RowTotal')?.value || 0;
+      return sum + rowTotal;
+    }, 0);
+  
+    this.details.get('Total')?.setValue(total);
+  }
+
 }
